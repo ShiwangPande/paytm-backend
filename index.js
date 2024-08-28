ca13f9cc95eb4544acdc6d31293c97ad4940d5d5
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-08-29 03:30:33 +0530

    paytm-backend

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors());
+// Define CORS options
+const corsOptions = {
+  origin: 'http://localhost:5173', // Allow only your frontend origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
+  credentials: true, // Allow credentials if needed
+};
+
+// Use CORS middleware with options
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
